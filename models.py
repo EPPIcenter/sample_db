be7f12761ffa549e3ebe28a01a80dfc8d9ac24b2
Max Murphy <maxwell.murphy@ucsf.edu>
2017-01-10 22:14:55 -0800

    spec, expand models

--- models.py
+++ models.py
@@ -14,27 +14,44 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sqlalchemy import Column, DateTime, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class Model(Base):
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    created = Column(DateTime, default=datetime.utcnow, nullable=False)
+    last_updated = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+
+
+class Study(Model):
+    __tablename__ = 'study'
+    title = Column(String, unique=True, index=True, nullable=False)
+    description = Column(String)
+    short_code = Column(String, unique=True, index=True, nullable=False)  # Should we constrain length?
+    longitudinal = Column(Boolean, nullable=False)
 
 
 class Sample(Model):
     __tablename__ = 'sample'
+    study_id = Column(Integer, ForeignKey('study.id'), index=True, nullable=False)
+    study = relationship('Study', backref="samples")
+    collection_date = Column(DateTime)  # Need validation against longitudinal study bool
 
 
 class MatrixPlate(Model):
     __tablename__ = 'matrix_plate'
-    label = Column(String, unique=True, index=True)
+    label = Column(String, unique=True, index=True, nullable=False)
 
 
 class MatrixTube(Model):
     __tablename__ = 'matrix_tube'
-    plate_id = Column(Integer, ForeignKey('matrix_plate.id'))
+    plate_id = Column(Integer, ForeignKey('matrix_plate.id'), index=True, nullable=False)
     plate = relationship('MatrixPlate', backref="tubes")
+    barcode = Column(String, unique=True, index=True, nullable=False)
+
---
+++ spec.md
@@ -0,0 +1,25 @@
+- Add barcoded samples to the database
+    - Linked to study information (year, site, contact person, sample type (DNA, serum, mosquito, etc))
+    - Option to have more than 1 DNA sample with the same name, differentiated as spot 1, spot 2 etc.
+        - Notes associated with sample, "second spot extracted"
+    - Where sample is located
+
+- Flag DNA Samples as exhausted
+
+- Search for location of DNA samples
+    - Flag exhausted samples
+    - Show when second sample is available
+
+- Way to verify that newly organized samples are the samples you wanted. Just verify that your new plate has the samples in the location where you said you wanted them.
+
+- Option to search for a single sample by typing in id and date info directly
+
+- Option to update location of samples after plates have been scanned. Can you do this by scanning the new plate and not having to scan all the plates from which samples had been taken? (yes)
+
+- Dates should read DD-MMM-YYYY to avoid ambiguity --> stored in db as DateTime value, this is purely a presentation and data parsing detail.
+
+- Automated backups --> daily when used?
+
+
+
+   
