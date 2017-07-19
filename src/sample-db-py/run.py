ad1b73e42eae7cc98f259c7f2a6ab0dd831650ea
Max Murphy <maxwell.murphy@ucsf.edu>
2017-07-18 17:40:57 -0700

    bug in windows resolving localhost

--- src/sample-db-py/run.py
+++ src/sample-db-py/run.py
@@ -2,7 +2,7 @@
 import flask as _
 from sample_db import app
 from gevent.wsgi import WSGIServer
-server = WSGIServer(('', 5000), app, error_log=app.logger)
+server = WSGIServer(('127.0.0.1', 5000), app, error_log=app.logger)
 app.logger.warning("Starting Server")
 server.serve_forever()
 # app.run()
